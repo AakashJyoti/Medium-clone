ea52436adcffec1a7ec62424449ee007e4912f4e
Aakashjyoti <Akjyoti572@gmail.com>
2022-07-02 17:00:12 +0530

    minnor change

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -169,6 +169,7 @@ function Post({ post }: Props) {
           />
         </form>
       )}
+      
       <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow  shadow-yellow-500">
         <h3 className="text-4xl">Comments</h3>
         <hr className="pb-2" />
